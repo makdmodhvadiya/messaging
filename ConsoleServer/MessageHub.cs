fa9f66a5c5613aca3e1ca086954e9ccffc1258ad
makdmodhvadiya <m.modhvadiya@gmail.com>
2016-05-04 04:55:09 +0530

    initial commit
    Created console server and web clients for real time message notifications

---
+++ ConsoleServer/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleServer
+{
+    public class MessageHub : Hub
+    {
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+    }
+}
---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server started on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
